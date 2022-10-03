2637634d172c4268700f1272106c58bbbc791763
SIVA SHANKER PS <112741003+SIVASHANKERPS@users.noreply.github.com>
2022-10-03 10:17:54 +0530

    Add files via upload

---
+++ Flask-app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+import random
+import json
+
+app  = Flask(__name__)
+PORT = 3009
+
+    
+@app.route("/", methods=["GET","POST"])
+def startpy():
+
+    result = {
+
+        "Greetings" : "hey guys"
+    }
+
+    return render_template("text.html", result = result)
+
+
+if __name__ == "__main__":
+    app.run( debug = True,host="0.0.0.0",port = PORT)
+    
